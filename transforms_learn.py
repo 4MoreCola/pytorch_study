96e4315500c9057d3d0c5f167d5cec511a9d7bfd
4MoreCola <liuweiglm@126.com>
2026-01-03 23:24:17 +0800

    study transforms

---
+++ transforms_learn.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+
+# python usage --> data type: tensor
+# transforms.
+
+img_path = "dataset/train/ants/0013035.jpg"
+img_path_abs = "F:\\py_torch projects\\dataset\\train\\ants\\0013035.jpg"
+img = Image.open(img_path)
+print(img)
+
+writer = SummaryWriter("logs")
+
+# 1. how to use transforms?
+tensor_trans = transforms.ToTensor()
+tensor_img = tensor_trans(img)
+print(tensor_img)
+
+# 2. why we need data type tensor?(what is the different between tensor and others data type?)
+writer.add_image("Tensor_img", tensor_img)
+writer.close()
